0c14f69417aba4a835e4e7c3f8a2768a1e06efec
Joan Barnes <jbarnes@dbasystems.com>
2021-08-01 11:01:47 -0500

    db OK

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Club
+from .models import Motel
+from .models import Event
+
+admin.site.register(Club)
+admin.site.register(Motel)
+admin.site.register(Event)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Club(models.Model):
+	name = models.CharField('Supper Club', max_length=120)
+	address = models.CharField('Address', max_length=300)
+	phone = models.CharField('Phone', max_length=20)
+	email = models.EmailField('Email', blank=True)
+	web = models.URLField('Web Site', blank=True)
+	info = models.TextField('Info', blank=True)
+
+	def __str__(self):
+		return self.name
+
+
+
+class Motel(models.Model):
+	name = models.CharField('Motel', max_length=120)
+	address = models.CharField('Address', max_length=300)
+	phone = models.CharField('Phone', max_length=20, blank=True)
+
+	def __str__(self):
+		return self.name
+
+
+
+class Event(models.Model):
+	name = models.CharField('Event Name', max_length=120)
+	event_date = models.DateTimeField('Event Date')
+	#club = models.CharField(max_length=120, blank=True)
+	club = models.ForeignKey('Club', blank=True, null=True, on_delete=models.SET_NULL)
+	#motel = models.CharField(max_length=120, blank=True)
+	motel = models.ForeignKey('Motel', blank=True, null=True, on_delete=models.SET_NULL)
+	comments = models.TextField('Comments', blank=True)
+
+	def __str__(self):
+		return self.name
+
